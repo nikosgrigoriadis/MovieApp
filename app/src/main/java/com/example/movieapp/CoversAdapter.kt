202089e86ac35d66520f1da118479a0b668301ad
nikosgrigoriadis <nikos678k@gmail.com>
2024-08-08 14:08:26 +0300

    Initial Commit
    data class MovieCover υπάρχει σε ξεχωριστό αρχείο
    Έχει δημιουργηθεί ένα HorizontalScrollView
    Χρήση recyclerView και ένα βήμα πριν την δημιουργία του RecyclerViewAdapter(CoversAdapter)

---
+++ app/src/main/java/com/example/movieapp/CoversAdapter.kt
@@ -0,0 +1,26 @@
+package com.example.movieapp
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class CoversAdapter(private val covers: List<MovieCover>) : RecyclerView.Adapter<CoversAdapter.CoversAdapterViewHolder>() {
+
+    class CoversAdapterViewHolder (itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CoversAdapterViewHolder {
+        TODO("Not yet implemented")
+
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: CoversAdapterViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+}
